94f48a89cbfaa0fa1d994e804bf4960eff745759
Shiva Sampath Lagisetty <shivalagisetty@Shivas-Mac-mini.local>
2025-02-17 03:03:53 +0530

    changes for GH pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-   base: "sourcemaps-decode"
+   base: "/sourcemaps-decode/"
   // build: {
   //   sourcemap: true // Enable sourcemaps for production build
   // }
